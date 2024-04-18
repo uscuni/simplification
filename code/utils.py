b6ec376a450edb68b6fdc4195225aa0c53160a2d
James Gaboardi <jgaboardi@gmail.com>
2024-04-18 09:11:00 -0400

    reusable utility functions (#35)
    * util funcs
    
    * add wuhan - #36
    
    * streamline osmnx & usecases

---
+++ code/utils.py
@@ -0,0 +1,56 @@
+import json
+import pathlib
+
+import geopandas
+import networkx
+
+__all__ = [
+    "city_fua",
+    "fua_city",
+    "read_sample_data",
+    "read_parquet_roads",
+    "graph_size",
+    "load_usecases",
+]
+
+
+ddir = pathlib.Path("..", "data")
+parq = "parquet"
+samp_parq = ddir / f"sample.{parq}"
+
+# dict of fua ID: cityname
+fua_city = {
+    1133: "Aleppo",
+    869: "Auckland",
+    4617: "Bucaramanga",
+    809: "Douala",
+    1656: "Liège",
+    4881: "Salt Lake City",
+    8989: "Wuhan",
+}
+
+# dict of cityname: fua ID
+city_fua = {c: f for f, c in fua_city.items()}
+
+
+def read_sample_data() -> geopandas.GeoDataFrame:
+    return geopandas.read_parquet(samp_parq)
+
+
+def read_parquet_roads(fua: int) -> geopandas.GeoDataFrame:
+    """Read OSM roads from parquet format; return bare columns."""
+    return geopandas.read_parquet(pathlib.Path(ddir, f"{fua}", f"roads_osm.{parq}"))[
+        ["highway", "geometry"]
+    ].reset_index(drop=True)
+
+
+def graph_size(info: str, g: networkx.Graph) -> str:
+    return f"{info}\n\t* {g}"
+
+
+def load_usecases(city: str) -> tuple[dict, pathlib.Path]:
+    """Load use cases for a city and also return the directory path."""
+    path_base = pathlib.Path("..", "usecases", str(city_fua[city]))
+    with open(path_base / "points.json") as f:
+        points = json.load(f)
+    return points, path_base

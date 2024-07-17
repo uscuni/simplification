c481f23f52f6fa2147d116896db1fdf72e1ded98
Martin Fleischmann <martin@martinfleischmann.net>
2024-07-17 17:10:12 +0200

    wip on typology

--- core/algorithms/common.py
+++ core/algorithms/common.py
@@ -1,8 +1,7 @@
 import momepy
-from libpysal import graph
 
 
-def continuity(roads):
+def continuity(roads, angle_threshold=120):
     """Assign COINS-based information to roads
 
     Parameters
@@ -17,26 +16,16 @@ def continuity(roads):
     """
     roads = roads.copy()
     # Measure continuity of street network
-    coins = momepy.COINS(roads, 120)
+    coins = momepy.COINS(roads, angle_threshold=angle_threshold, flow_mode=True)
 
     # Assing continuity group
-    roads["coins_group"] = coins.stroke_attribute()
+    group, end = coins.stroke_attribute()
+    roads["coins_group"] = group
+    roads["coins_end"] = end
 
     # Assign length of each continuity group and a number of segments within the group.
     coins_grouped = roads.length.groupby(roads.coins_group)
     roads["coins_len"] = coins_grouped.sum()[roads.coins_group].values
     roads["coins_count"] = coins_grouped.size()[roads.coins_group].values
 
-    # Figure out which segments are on the ends of their continuity groups.
-    roads_contiguity = graph.Graph.build_contiguity(
-        roads, rook=False
-    ).assign_self_weight()
-    grouper = roads.coins_group.take(
-        roads_contiguity._adjacency.index.codes[1]
-    ).groupby(roads_contiguity._adjacency.index.codes[0])
-    coins_end = []
-    for i, g in grouper:
-        coins_end.append((g == g.loc[i]).sum() in (1, 2))
-    roads["coins_end"] = coins_end
-
-    return roads
+    return roads, coins

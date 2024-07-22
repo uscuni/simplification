6c67f693902397095549ce68190e6c8e2a1b01e1
James Gaboardi <jgaboardi@gmail.com>
2024-07-21 20:02:07 -0400

    add small tolerance for dev - plot_case

--- core/tests/test_viz_protocol.py
+++ core/tests/test_viz_protocol.py
@@ -1,11 +1,16 @@
 import geopandas
+import pytest
 import shapely
 from matplotlib.testing.decorators import image_comparison
 
 import core
 
 
-@image_comparison(baseline_images=["test_plot_case.png"], style="mpl20")
+@image_comparison(
+    baseline_images=["test_plot_case.png"],
+    style="mpl20",
+    tol=6.1 if pytest.ENV_TYPE == "dev" else 1,
+)
 def test_plot_case():
     p1, p2 = shapely.Point(20, 20), shapely.Point(21, 21)
     lines = geopandas.GeoDataFrame(geometry=[shapely.LineString((p1, p2))])

6a152d635dde5743fc9db40720b303990196fa05
Martin Fleischmann <martin@martinfleischmann.net>
2024-08-14 10:25:39 +0200

    include exclusion_mask (#159)

--- core/algorithms/simplify.py
+++ core/algorithms/simplify.py
@@ -436,6 +436,8 @@ def simplify_network(
     area_threshold_circles=5e4,
     isoareal_threshold_circles=0.75,
     eps=1e-4,
+    exclusion_mask=None,
+    predicate="intersects",
 ):
     # Merge nearby nodes (up to double of distance used in skeleton).
     roads = consolidate_nodes(roads, tolerance=max_segment_length * 2.1)
@@ -447,6 +449,8 @@ def simplify_network(
         isoareal_threshold_blocks=isoareal_threshold_blocks,
         area_threshold_circles=area_threshold_circles,
         isoareal_threshold_circles=isoareal_threshold_circles,
+        exclusion_mask=exclusion_mask,
+        predicate=predicate,
     )
 
     # Loop 1
@@ -469,6 +473,8 @@ def simplify_network(
         isoareal_threshold_blocks=isoareal_threshold_blocks,
         area_threshold_circles=area_threshold_circles,
         isoareal_threshold_circles=isoareal_threshold_circles,
+        exclusion_mask=exclusion_mask,
+        predicate=predicate,
     )
 
     # Loop 2

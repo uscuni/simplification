212b69887f9f53c482f82b0e3bd7582e35b401d7
anvy <anvy@itu.dk>
2024-12-01 12:21:25 +0100

    update regex

--- notebooks/methods/parenx-run.sh
+++ notebooks/methods/parenx-run.sh
@@ -6,7 +6,7 @@ FOLDERS=$(find ./data -type d -mindepth 2)
 # run skeletonization and voronoi for each subfolder
 for folder in $FOLDERS
 do
-	if [[ $folder == *parenx* ]] ; then
+	if [[ $folder == *temp-parenx* ]] ; then
 		echo "Simplification for $folder started";
 		skeletonize.py $folder"/roads_osm.gpkg" $folder"/skeletonize.gpkg"
 		voronoi.py $folder"/roads_osm.gpkg" $folder"/voronoi.gpkg"

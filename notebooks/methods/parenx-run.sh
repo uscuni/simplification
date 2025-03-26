557dca0c72764ed30bfd622a1821b954416b2acf
Martin Fleischmann <martin@martinfleischmann.net>
2025-03-26 09:15:07 +0100

    update neatnet results, measure performance (#254)
    * update neatnet
    
    * update neatnet results, measure performance
    
    * lock
    
    * update expected
    
    * make ruff happy
    
    * unhappy bastard...

--- notebooks/methods/parenx-run.sh
+++ notebooks/methods/parenx-run.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 # getting subfolders in temp-parenx
-FOLDERS=$(find ./data -type d -mindepth 2)
+FOLDERS=$(find ./data -mindepth 2 -type d)
 
 # run skeletonization and voronoi for each subfolder
 for folder in $FOLDERS
 do
 	if [[ $folder == *temp-parenx* ]] ; then
 		echo "Simplification for $folder started";
-		skeletonize.py $folder"/roads_osm.gpkg" $folder"/skeletonize.gpkg"
-		voronoi.py $folder"/roads_osm.gpkg" $folder"/voronoi.gpkg"
+        
+		echo "Running skeletonize.py..."
+		/usr/bin/time -v skeletonize.py $folder"/roads_osm.gpkg" $folder"/skeletonize.gpkg" 2> ${folder}/skeletonize_mem.log
+		echo "Skeletonize peak memory: $(grep "Maximum resident set size" ${folder}/skeletonize_mem.log | awk '{print $6}') kB"
+
+		echo "Running voronoi.py..."
+		/usr/bin/time -v voronoi.py $folder"/roads_osm.gpkg" $folder"/voronoi.gpkg" 2> ${folder}/voronoi_mem.log
+		echo "Voronoi peak memory: $(grep "Maximum resident set size" ${folder}/voronoi_mem.log | awk '{print $6}') kB"
 	fi
 done
 
